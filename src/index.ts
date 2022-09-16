a805fdf60c12358acd81000d3d745f0762b5a34f
Salief Lewis <salieflewis@gmail.com>
2022-09-16 13:52:52 -0400

    lite config

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ const config: TwitterOptions = {
   version: '2',
   extension: false,
   consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
-  // access_token_key:
-  // access_token_secret:
+  consumer_secret: process.env
+    .TWITTER_CONSUMER_SECRET as string,
+  access_token_key: process.env
+    .TWITTER_ACCESS_TOKEN_KEY as string,
+  access_token_secret: process.env
+    .TWITTER_ACCESS_TOKEN_SECRET as string,
 };
 
-const twitter = new Twitter(config)
-
-// bearer_token: process.env.TWITTER_BEARER_TOKEN
+const twitter = new Twitter(config);
