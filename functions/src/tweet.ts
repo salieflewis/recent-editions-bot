80f69d8c921de041841b2f0201f52b54a3b85aca
Salief Lewis <salieflewis@gmail.com>
2022-10-24 12:49:57 -0400

    implemented lookupAddress

--- functions/src/tweet.ts
+++ functions/src/tweet.ts
@@ -1,4 +1,5 @@
 import { TwitterApi } from 'twitter-api-v2';
+import { ethers } from 'ethers';
 import * as dotenv from 'dotenv'; // https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 
@@ -11,13 +12,20 @@ const userClient = new TwitterApi({
 const v2Client = userClient.v2;
 
 const MINTER_STRING = 'Latest mint: https://create.zora.co/editions/';
+const CREATOR_STRING = 'Created by: ';
 const SIZE_STRING = '\nEdition size: ';
 
-const createTweet = async (address: string, size: number) => {
+const provider = ethers.getDefaultProvider();
+
+const createTweet = async (address: string, creator: string, size: number) => {
   try {
     await v2Client.tweet(
       MINTER_STRING +
         address +
+        CREATOR_STRING +
+        ((await provider.lookupAddress(creator)) != null
+          ? provider.lookupAddress(creator)
+          : creator) +
         SIZE_STRING +
         (size == 18446744073709551615 ? 'OPEN EDITION' : size)
     );
